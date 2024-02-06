b39f2dd1cbd9547126a4c397b58d0abcc6429f89
Victor Olatunji <victoluolatunji@gmail.com>
2024-02-06 23:18:14 +0100

    update

--- server.js
+++ server.js
@@ -11,32 +11,51 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-// HANDLE GET REQUESTS
+// handle post search request
 
-app.get("/users", (req, res) => {
-  const users = [
-    { id: 1, name: "John Doe", age: 30 },
-    { id: 2, name: "Jane Doe", age: 25 },
-    { id: 3, name: "Bob Smith", age: 40 },
-    { id: 4, name: "Alice Johnson", age: 35 },
-    { id: 5, name: "Mike Brown", age: 45 },
-    { id: 6, name: "Sarah Davis", age: 32 },
-    { id: 7, name: "Tom Wilson", age: 28 },
-    { id: 8, name: "Emily Anderson", age: 27 },
-    { id: 9, name: "David Taylor", age: 26 },
-    { id: 10, name: "Lisa Miller", age: 29 },
-    { id: 11, name: "Richard Wilson", age: 31 },
-    { id: 12, name: "Karen Martinez", age: 33 },
-    { id: 13, name: "Jacob Thompson", age: 34 },
-    { id: 14, name: "Amanda Garcia", age: 36 },
-    { id: 15, name: "Daniel Hernandez", age: 37 },
-    { id: 16, name: "Michael Rodriguez", age: 38 },
-    { id: 17, name: "Emily Martinez", age: 39 },
-  ];
+const contact = [
+  { name: "John Doe", email: "john@example.com" },
+  { name: "Jane Doe", email: "jane@example.com" },
+  { name: "Bob Smith", email: "bob@example.com" },
+  { name: "Alice Johnson", email: "alice@example.com" },
+  { name: "Mike Brown", email: "mike@example.com" },
+  { name: "Sara Lee", email: "sara@example.com" },
+  { name: "David Kim", email: "david@example.com" },
+];
 
-  res.send(`
-  <h1 class="text-2xl font-bold my-4">Users</h1>
-    <ul>${users.map((user) => `<li>${user.name}</li>`).join("")}</ul>`);
+app.post("/search", (req, res) => {
+  const searchTerm = req.body.search.toLowerCase();
+
+  if (!searchTerm) {
+    return res.send("<tr></tr>");
+  }
+  const searchResults = contact.filter((contact) => {
+    const name = contact.name.toLowerCase();
+    const email = contact.email.toLowerCase();
+
+    return name.includes(searchTerm) || email.includes(searchTerm);
+  });
+
+  
+  
+  setTimeout(()=>{
+    
+    const searchResultHtml = searchResults
+      .map(
+        (contact) => `
+    <tr>
+  
+    <td><div class="my-4 p-2>${contact.name}</div</td>
+    <td><div class="my-4 p-2>${contact.email}</div</td>
+    </tr>
+    
+    `
+      )
+      .join("");
+
+      
+      res.send(searchResultHtml);
+    }, 1000)
 });
 
 // Start the server
