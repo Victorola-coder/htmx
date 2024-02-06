fce2e6312e9218cd5309026152c379d48e9fd8fd
Victor Olatunji <victoluolatunji@gmail.com>
2024-02-06 23:56:34 +0100

    [feat] temp

--- server.js
+++ server.js
@@ -11,32 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-// HANDLE GET REQUESTS
+// handle weather request
 
-app.get("/users", (req, res) => {
-  const users = [
-    { id: 1, name: "John Doe", age: 30 },
-    { id: 2, name: "Jane Doe", age: 25 },
-    { id: 3, name: "Bob Smith", age: 40 },
-    { id: 4, name: "Alice Johnson", age: 35 },
-    { id: 5, name: "Mike Brown", age: 45 },
-    { id: 6, name: "Sarah Davis", age: 32 },
-    { id: 7, name: "Tom Wilson", age: 28 },
-    { id: 8, name: "Emily Anderson", age: 27 },
-    { id: 9, name: "David Taylor", age: 26 },
-    { id: 10, name: "Lisa Miller", age: 29 },
-    { id: 11, name: "Richard Wilson", age: 31 },
-    { id: 12, name: "Karen Martinez", age: 33 },
-    { id: 13, name: "Jacob Thompson", age: 34 },
-    { id: 14, name: "Amanda Garcia", age: 36 },
-    { id: 15, name: "Daniel Hernandez", age: 37 },
-    { id: 16, name: "Michael Rodriguez", age: 38 },
-    { id: 17, name: "Emily Martinez", age: 39 },
-  ];
+let curentTemp = 20;
 
-  res.send(`
-  <h1 class="text-2xl font-bold my-4">Users</h1>
-    <ul>${users.map((user) => `<li>${user.name}</li>`).join("")}</ul>`);
+app.get("/get-temp", (req, res) => {
+  curentTemp += Math.random() * 2 - 1;
+
+  res.send(curentTemp.toFixed(1) + "C");
 });
 
 // Start the server
